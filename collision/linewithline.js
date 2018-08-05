71230ff85e29c55dc74400c239e5877a4d7ca000
falseness <40433365+falseness@users.noreply.github.com>
2018-08-05 11:27:35 +0300

    Add files via upload

---
+++ collision/linewithline.js
@@ -0,0 +1,55 @@
+function pointOnLine(x, x1, x2)
+{
+    return ((x < x1 && x2 < x) || (x < x2 && x1 < x))
+}
+function collisionVerticalWithLine(vertical, line)
+{
+    let y = line.k  * vertical.x + line.b
+    return (pointOnLine(y, vertical.y1, vertical.y2) && pointOnLine(vertical.x, line.x1, line.x2))
+}
+function collisionVerticalWithVertical(vertical1, vertical2)
+{
+    return (vertical1.x == vertical2.x && 
+            (pointOnLine(vertical1.y1, vertical2.y1, vertical2.y2) || 
+             pointOnLine(vertical1.y2, vertical2.y1, vertical2.y2) || 
+             pointOnLine(vertical2.y1, vertical1.y1, vertical1.y2) ||
+             pointOnLine(vertical2.y2, vertical1.y1, vertical1.y2)))
+}
+function collisionLineWithLine(line1, line2)
+{
+    
+    if (line1.k != line2.k)
+    {
+        let x = (line2.b - line1.b) / (line1.k - line2.k)
+        let y = line1.k * x + line1.b
+        if (pointOnLine(x, line1.x1, line1.x2) && pointOnLine(x, line2.x1, line2.x2))
+        {
+            //&& pointOnLine(y, line1.y1, line2.y2) && pointOnLine(x, line2.y1, line2.y2))
+            return true
+        }
+    }
+    return false
+}
+function lineFormula(x1, y1, x2, y2)
+{
+    if (x1 == x2)
+    {
+        return {type: 'vertical', x: x1, y1: y1, y2: y2}
+    }
+    else
+    {
+        let k = ((y1 - y2) / (x1 - x2))
+        return {type: 'line', x1: x1, x2: x2, y1: y1, y2: y2, k: k, b: (y1 - k * x1)}
+    }
+}
+function linesCollision(line1, line2)
+{
+    if (line1.type == 'line' && line2.type == 'line')
+        return collisionLineWithLine(line1, line2)
+    else if (line1.type == 'vertical' && line2.type == 'line')
+        return collisionVerticalWithLine(line1, line2)
+    else if (line1.type == 'line' && line2.type == 'vertical')
+        return collisionVerticalWithLine(line2, line1)
+    else
+        return collisionVerticalWithVertical(line1, line2)
+}
