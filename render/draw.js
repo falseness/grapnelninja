f7a93d5c2b15cd87e24e01547f0602c5938604ed
Falseness <baharevuniversal@gmail.com>
2026-06-10 23:08:55 +0000

    TASK-003: split rendering into named layers

--- render/draw.js
+++ render/draw.js
@@ -1,34 +1,57 @@
- function draw()
+function drawBackgroundLayer()
 {
     visualEffects.background.draw()
-    
-    ctx.scale(scale[version], scale[version])
-    
-    const gameState = visualEffects.getGameState()
-    visualEffects.screenEffects.begin(gameState)
+}
+
+function drawLightsLayer(gameState)
+{
     visualEffects.lightmap.clear(gameState)
     visualEffects.lightmap.draw(gameState)
     visualEffects.lightmap.composite(gameState)
-    visualEffects.playerTrail.draw(gameState)
-    visualEffects.particles.update(gameState)
+}
 
+function drawWorldLayer()
+{
     grapnel.draw()
-    
-    
+
     for (let i = 1; i < floors.length - 1; ++i)
     {
         floors[i].draw()
     }
     floors[0].draw()
     floors[floors.length - 1].draw()
-    
-    
+
     screen.draw()
     ninja.draw()
+}
+
+function drawParticlesAndTrailsLayer(gameState)
+{
+    visualEffects.playerTrail.draw(gameState)
+    visualEffects.particles.update(gameState)
     visualEffects.particles.draw(gameState)
-    
+}
+
+function drawUILayer(gameState)
+{
     visualEffects.ui.draw(gameState)
+}
+
+function draw()
+{
+    drawBackgroundLayer()
+
+    ctx.scale(scale[version], scale[version])
+
+    const gameState = visualEffects.getGameState()
+    visualEffects.screenEffects.begin(gameState)
+
+    drawLightsLayer(gameState)
+    drawWorldLayer()
+    drawParticlesAndTrailsLayer(gameState)
+    drawUILayer(gameState)
+
     visualEffects.screenEffects.end(gameState)
-    
+
     ctx.scale(1 / scale[version], 1 / scale[version])
 }
